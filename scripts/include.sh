a85df793ae9e6cbb26bdb791020f3172020e7adc
Sven Willner <sven.willner@gmail.com>
2018-07-08 13:50:01 +0200

    Various fixes and cleanup

--- scripts/include.sh
+++ scripts/include.sh
@@ -1,18 +1,49 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2034
+COLOR_BLACK='\033[0;30m'
+COLOR_DARK_GRAY='\033[1;30m'
+COLOR_RED='\033[0;31m'
+COLOR_LIGHT_RED='\033[1;31m'
+COLOR_GREEN='\033[0;32m'
+COLOR_LIGHT_GREEN='\033[1;32m'
+COLOR_ORANGE='\033[0;33m'
+COLOR_YELLOW='\033[1;33m'
+COLOR_BLUE='\033[0;34m'
+COLOR_LIGHT_BLUE='\033[1;34m'
+COLOR_PURPLE='\033[0;35m'
+COLOR_LIGHT_PURPLE='\033[1;35m'
+COLOR_CYAN='\033[0;36m'
+COLOR_LIGHT_CYAN='\033[1;36m'
+COLOR_LIGHT_GRAY='\033[0;37m'
+COLOR_WHITE='\033[1;37m'
+COLOR_CLEAR='\033[0m'
+
+info () {
+    echo -e "$COLOR_LIGHT_BLUE$1$COLOR_CLEAR"
+}
+
+error () {
+    echo -e "$COLOR_RED$1$COLOR_CLEAR" 1>&2
+    exit "${2:-1}"
+}
+
 ask_user () {
+    local question=$1
+    local default=$2
     local default_indicator
-    if [ "$2" = "y" ]
+    if [ "$default" = "y" ]
     then
         default_indicator="[Y/n]"
     else
         default_indicator="[y/N]"
     fi
     local response
-    read -r -p "$1 $default_indicator " response
+    echo -e -n "$COLOR_ORANGE$question$COLOR_CLEAR $COLOR_LIGHT_GRAY$default_indicator$COLOR_CLEAR "
+    read -r response
     case $response in
         [yY][eE][sS]|[yY]|'')
-            if [ "$2" != "y" ] && [ "$response" == "" ]
+            if [ "$default" != "y" ] && [ "$response" == "" ]
             then
                 return 1
             else
@@ -23,6 +54,26 @@ ask_user () {
             return 1
             ;;
     esac
+}
+
+ensure_link () {
+    local from=$1
+    local to=$2
+    if [ ! -e "$to" ]
+    then
+        ln -s "$from" "$to"
+    else
+        real=$(realpath "$to")
+        if [ "$real" != "$from" ]
+        then
+            if [ "$real" == "$to" ]
+            then
+                ask_user "Replace $to by link to $from?" "y"
+            else
+                ask_user "Replace $to by link to $from (currently to $real)?" "y"
+            fi
+        fi
+    fi
 }
 
 ensure_command () {
